64159901df04ee522d2e393c17b83f9d70e7fcb9
Lana <rus_lana@bk.ru>
2023-01-18 00:10:48 +0500

    formate

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -2,14 +2,14 @@ package ru.netology.sqr;
 
 public class SQRService {
     public int numberSqr(int under, int up) {
-            int n = 0;
-            for (int i = 10; i <= 99; i++) {
-                if (under <= i * i & i * i <= up) {
-                    n = n + 1;
-                }
+        int n = 0;
+        for (int i = 10; i <= 99; i++) {
+            if (under <= i * i & i * i <= up) {
+                n = n + 1;
             }
-            return n;
         }
+        return n;
+    }
 
 
 }
--- src/test/java/SQRServiceTest.java
+++ src/test/java/SQRServiceTest.java
@@ -10,6 +10,7 @@ public class SQRServiceTest {
         int actual = service.numberSqr(10, 99);
         Assertions.assertEquals(expected, actual);
     }
+
     @Test
     public void testUpperRange() {
         SQRService service = new SQRService();
@@ -17,6 +18,7 @@ public class SQRServiceTest {
         int actual = service.numberSqr(10000, 999999);
         Assertions.assertEquals(expected, actual);
     }
+
     @Test
     public void testBoundaryValues() {
         SQRService service = new SQRService();
@@ -24,6 +26,7 @@ public class SQRServiceTest {
         int actual = service.numberSqr(100, 9801);
         Assertions.assertEquals(expected, actual);
     }
+
     @Test
     public void testInRange() {
         SQRService service = new SQRService();
@@ -31,6 +34,7 @@ public class SQRServiceTest {
         int actual = service.numberSqr(200, 300);
         Assertions.assertEquals(expected, actual);
     }
+
     @Test
     public void testError() {
         SQRService service = new SQRService();
